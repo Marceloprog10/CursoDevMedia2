44cd8df2997fc71e0a253bc914f00c8fc545b347
Marceloprog10 <87679433+Marceloprog10@users.noreply.github.com>
2021-10-21 00:39:09 -0300

    Novo curso de Java Script.

---
+++ JavaScript/Exercicios01.js
@@ -0,0 +1,63 @@
+var Oi = "Hello World Novo Marcelo";
+function ImprimirTexto(Texto){
+    console.log(Texto);
+}
+
+ImprimirTexto(Oi);
+
+/*--------------------------------------------------*/
+var numero = 9;
+console.log(numero++);
+console.log(numero);
+
+/*--------------------------------------------------*/
+var graus_celsius = 15;
+var graus_fahrenheit = (graus_celsius * 9/5 ) + 32;
+console.log(graus_celsius + "°C equivalem a " + graus_fahrenheit + "°F" );
+
+/*--------------------------------------------------*/
+var idade = 18;
+var atendeAClassificacaoEtaria = idade >= 18;
+console.log(atendeAClassificacaoEtaria);
+
+/*--------------------------------------------------*/
+var materia = null;
+console.log(materia == null);
+
+/*--------------------------------------------------*/
+var preco = 10;
+var desconto = 0.1;
+var novo_preco = preco - ( ( preco / desconto ) / 100 );
+console.log(novo_preco);
+
+/*--------------------------------------------------*/
+var situacao;
+console.log(situacao);
+
+/*--------------------------------------------------*/
+var avaliacao_1 = 6;
+var avaliacao_2 = 5;
+var media = (avaliacao_1 + avaliacao_2) / 2;
+console.log("A média do aluno é igual a: " + media);
+
+/*--------------------------------------------------*/
+var forca; // N
+var massa = 2; // kg
+var aceleracao = 6; // m/s²
+forca = massa * aceleracao;
+console.log("O valor da força é de " + forca + "N");
+
+/*--------------------------------------------------*/
+var preco = 10;
+preco += 20;
+console.log(preco);
+/*--------------------------------------------------*/
+console.log( "1" + 1 );
+console.log( 15 % 2 );
+console.log( true + true );
+
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
