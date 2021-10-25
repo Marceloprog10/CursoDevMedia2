618011be8bc7bcb85b02519a9e1242c40b2aa3c6
Marceloprog10 <87679433+Marceloprog10@users.noreply.github.com>
2021-10-25 15:26:06 -0300

    Exercicios gerais.

--- JavaScript/Exercicios01.js
+++ JavaScript/Exercicios01.js
@@ -57,7 +57,97 @@ console.log( 15 % 2 );
 console.log( true + true );
 
 /*--------------------------------------------------*/
+var produto = {
+    id: 9,
+    nome: "Cafeteira Elétrica",
+    valor: 99.00
+};
+console.log(produto.nome);
+
+var pessoa = {
+    cpf: 0,
+    nome: ""
+};
+pessoa.cpf = 06997102948;
+pessoa.nome = "Marcelo de Matos";
+console.log(pessoa);
 
 /*--------------------------------------------------*/
+function teste(Apessoa) {
+    return Apessoa.nome;
+};
+
+console.log(teste(pessoa));
+
+/*--------------------------------------------------*/
+var Alunos = [
+    {id: 1, nome: "Marcelo", idade: 32},
+    {id: 2, nome: "Pedro", idade: 20},
+    {id: 3, nome: "João", idade: 2}
+];
+
+var aluno = 0;
+for (aluno in Alunos) {
+    console.log(Alunos[aluno]);
+};
+
+/*--------------------------------------------------
+//Método para sortear 49 itens.
+var quant = 0;
+while (quant <= 50) { 
+    var Item = Math.floor(Math.random() * 50);    
+    console.log(quant + '  ' + Item);    
+    quant++;
+};*/
+console.log("");
+console.log("");
+console.log("");
+
+/*--------------------------------------------------*/
+var colecao_animes = [
+    { id: 1, titulo: "Naruto",      episodios: 673 },
+    { id: 2, titulo: "Bleach",      episodios: 366 },
+    { id: 3, titulo: "One Piece",   episodios: 931 },
+];
+
+var primeiro_anime = colecao_animes[1];
+
+var id_anime = primeiro_anime.id;
+var titulo_anime = primeiro_anime.titulo;
+var total_episodios_anime = primeiro_anime.episodios;
+
+console.log("Id do anime: " + id_anime);
+console.log("Título do anime: " + titulo_anime);
+console.log("Total de episódios do anime: " + total_episodios_anime);
+
+const url = "https://www.site.com.br";
+console.log(url);
+
+/*--------------------------------------------------*/
+var colecao_livros = [
+    { id: 1, nome: "Dom Quixote",           numero_paginas: 863 },
+    { id: 2, nome: "O Senhor dos Anéis",    numero_paginas: 1200 },
+    { id: 3, nome: "As Crônicas de Nárnia", numero_paginas: 752 },
+    { id: 4, nome: "O Caçador de Pipas",    numero_paginas: 371 },
+    { id: 5, nome: "A Montanha Mágica",     numero_paginas: 848 }
+];
+
+var total_livros = colecao_livros.length;
+primeiro_livro = colecao_livros[0];
+ultimo_livro = colecao_livros[4];
+console.log( "O total de livros é: " + total_livros );
+console.log( "O nome do primeiro livro da coleção é: " + primeiro_livro.nome );
+console.log( "O nome do último livro da coleção é: " + ultimo_livro.nome );
+
+/*--------------------------------------------------*/
+var x = "";
+var y = 0;
+
+if (x == y) {
+    console.log("x e y iguais");  
+}
+else {
+    console.log("x e y diferentes");
+}
 
 /*--------------------------------------------------*/
