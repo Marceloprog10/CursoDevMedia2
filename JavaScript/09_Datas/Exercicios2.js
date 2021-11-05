f4743a216337ed119b1beb79991830d2c27079b9
Marceloprog10 <87679433+Marceloprog10@users.noreply.github.com>
2021-11-05 00:07:40 -0300

    Incluídas anotações.

---
+++ JavaScript/09_Datas/Exercicios2.js
@@ -0,0 +1,83 @@
+const hoje = new Date();
+const diferencaHoras = hoje.getUTCMinutes() /60;
+console.log('Diferença de horário: ' + diferencaHoras + ' horas');
+
+var a = 'teste';
+a.length
+
+/*-----------------------------------------------*/
+console.log("\n");
+
+const plataforma = 'Mobile';
+let msg = "";
+
+switch (plataforma) {
+  case 'Mobile':
+    msg = 'React Native e Flutter';
+    break;
+
+  case 'Web':
+    msg = 'Angular, React e Vue.js';
+     break;
+
+  case 'Back-end':
+      msg = 'Laravel, .NET e Spring';
+      break;
+
+default:
+    msg = 'Plataforma não disponível';
+}
+
+console.log(msg);
+
+/*-----------------------------------------------*/
+console.log("\n");
+const hoje2 = new Date();
+hoje2.setHours(20);
+hoje2.setMinutes(0);
+hoje2.setSeconds(0);
+
+console.log(hoje2.toLocaleTimeString());
+
+/*-----------------------------------------------*/
+console.log("\n");
+const ingredientes = ['açúcar', 'farinha de trigo', 'ovo'];
+
+for (index in ingredientes){
+    console.log(index + ' - ' + ingredientes[index]);
+}
+
+/*-----------------------------------------------*/
+console.log("\n");
+const dataAtual = new Date();
+
+const diaSemana = dataAtual.getDay();
+const diaMes = dataAtual.getDate();
+
+if (diaSemana == 5 && diaMes == 13) {
+  console.log('Cuidado! Hoje é sexta-feira 13');
+} else if (diaMes == 13) {
+  console.log('Sextou!!!');
+} else {
+    console.log('Outro dia');
+}
+
+/*-----------------------------------------------
+const 
+hoje3 = new Date();
+const diferencaHoras3 = hoje3.getUTCMinutes() / 60;
+
+console.log('Diferença de horário: ' + diferencaHoras3 + ' horas');*/
+
+const totalAcertos = 4;
+const primeiroAcerto = new Date('2020/10/05');
+let index4 = 0;
+do {
+  console.log( primeiroAcerto.toDateString() );
+
+  let dia = primeiroAcerto.getDate();
+  dia++;
+  primeiroAcerto.setDate(dia);
+
+  index4++;
+} while (index4 < totalAcertos);
