8afad976c1ef909b743fb9c95142afcaf2d58539
Marceloprog10 <87679433+Marceloprog10@users.noreply.github.com>
2021-12-01 00:29:21 -0300

    mais exercicios.

--- JavaScript/12_Modulos_Import_Export/Exercicios3_Signos/main.js
+++ JavaScript/12_Modulos_Import_Export/Exercicios3_Signos/main.js
@@ -6,10 +6,13 @@ const nome_signo = retorna_signo(colecao_signos, data_app);
 console.log("O signo de hoje é: " + nome_signo);
 
 
-let string = "Estou aprendendo1JavaScript na DevMedia";
-let resultado = string.substring(6,16);
+/* -- para resolver um exercicio que estou só errando 
 
-console.log(resultado);
+import colecao_signos from './dados/dados.js';
+import retorna_signo from './funcoes/funcoes.js';
 
-let soma = 12;
-console.log("A soma entre os números é: " soma);
+let data_app = new Date();
+
+const nome_signo = retorna_signo(data_app, colecao_signos);
+
+console.log("O signo de hoje é: " + nome_signo);*/
