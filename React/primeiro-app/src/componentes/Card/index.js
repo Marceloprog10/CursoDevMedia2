3dc86e588c9cc638e1a84a7255acaf4cb5af54cf
marcelo <marceloprog10@gmail.com>
2022-02-01 00:19:02 -0300

    Criacao de pagina usando react Header/Home/Footer

---
+++ React/primeiro-app/src/componentes/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function Card (){
+    return (
+        <div className='card-container'>
+            <p>Card - Componentes:</p>
+            <p>Facilita em manter partes menores funcionando corretamente</p>
+            <p>Voce pode reutiliza-los, ou seja, menos codigo para se escrever</p>
+        </div>
+    );
+}
---
+++ React/primeiro-app/src/componentes/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+function Header(){
+    return (
+        <header>Header - Trabalhando com componentes</header>
+    )
+}
+
+export default Header;
